20c47f631570564e8f0e7bc47decf1592248c5ae
Kerry Buckley <kerryjbuckley@gmail.com>
2020-12-10 13:16:53 +0000

    New project

---
+++ lib/where_to_visit.ex
@@ -0,0 +1,18 @@
+defmodule WhereToVisit do
+  @moduledoc """
+  Documentation for `WhereToVisit`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WhereToVisit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/where_to_visit_test.exs
@@ -0,0 +1,8 @@
+defmodule WhereToVisitTest do
+  use ExUnit.Case
+  doctest WhereToVisit
+
+  test "greets the world" do
+    assert WhereToVisit.hello() == :world
+  end
+end
